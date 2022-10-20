9123c93234a7267311dcf8ba1a0ce6703c9b4bab
Nikita <nikitaleonov00@mail.ru>
2022-10-20 19:49:33 +0300

    done

---
+++ helpers/args.helper.js
@@ -0,0 +1,15 @@
+export const getArgs = (args) => {
+    const res = {}
+    const [, , ...rest] = args
+    rest.forEach((value, index, array) => {
+        if (value.charAt(0) === '-') {
+            if (index === array.length - 1 || array[index + 1].charAt(0) === '-') {
+                res[value.substring(1)] = true
+            } else {
+                res[value.substring(1)] = array[index + 1]
+            }
+        }
+    })
+    return res
+}
+
